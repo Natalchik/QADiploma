b390083c20ac587efc251c47d15f894db0728d6d
Nataly Kalinina <annataly-1238@ya.ru>
2023-04-19 12:08:03 +0300

    SQLRequest edited

--- src/test/java/ru/netology/data/SQLRequests.java
+++ src/test/java/ru/netology/data/SQLRequests.java
@@ -7,52 +7,36 @@ import org.apache.commons.dbutils.handlers.ScalarHandler;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+
 
 public class SQLRequests {
         private static final QueryRunner runner = new QueryRunner();
-        private static final Connection conn = getConnection();
 
+     @SneakyThrows
         public static Connection getConnection() {
-            try {
-                return DriverManager.getConnection(System.getProperty("db.url"), System.getProperty("db.user"), System.getProperty("db.password"));
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
+            return DriverManager.getConnection(System.getProperty("db.url"), System.getProperty("db.user"), System.getProperty("db.pass"));
             }
-        }
 
+     @SneakyThrows
         public static void clearTables() {
-            QueryRunner runner = new QueryRunner();
-
-            String clearCreditRequestTableQuery = "DELETE FROM credit_request_entity;";
             String clearOrderTableQuery = "DELETE FROM order_entity;";
             String clearPaymentTableQuery = "DELETE FROM payment_entity;";
-
-            try (
-                    Connection conn = DriverManager.getConnection(
-                            System.getProperty("db.url"),
-                            System.getProperty("db.user"),
-                            System.getProperty("db.pass")
-                    )
-            ) {
-                runner.update(conn, clearCreditRequestTableQuery);
-                runner.update(conn, clearOrderTableQuery);
-                runner.update(conn, clearPaymentTableQuery);
-            } catch (SQLException e) {
-                e.printStackTrace();
+            String clearCreditRequestTableQuery = "DELETE FROM credit_request_entity;";
+            runner.update(getConnection(), clearOrderTableQuery);
+            runner.update(getConnection(), clearPaymentTableQuery);
+            runner.update(getConnection(), clearCreditRequestTableQuery);
             }
-        }
 
         @SneakyThrows
         public static String getStatusByCard() {
             String status = "SELECT status FROM payment_entity ORDER BY created DESC";
-            return runner.query(conn, status, new ScalarHandler<>());
+            return runner.query(getConnection(), status, new ScalarHandler<>());
             }
 
         @SneakyThrows
         public static String getStatusByCredit() {
             String status = "SELECT status FROM credit_request_entity ORDER BY created DESC";
-            return runner.query(conn, status, new ScalarHandler<>());
+            return runner.query(getConnection(), status, new ScalarHandler<>());
             }
 
     }
